1085d745d6adc61edab3cdb4e2dc5b86b289e493
metasci <believewithyoureyes@gmail.com>
2020-05-08 17:11:29 -0400

    More angular stuff

--- dash/dash-public/src/modules/app/app.module.ts
+++ dash/dash-public/src/modules/app/app.module.ts
@@ -2,14 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app/app.component';
+import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MaterialModule } from '../material/material.module';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { NavTabsComponent } from './nav-bar/nav-tabs/nav-tabs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    NavTabsComponent
   ],
   imports: [
     BrowserModule,
