17219271b8235caf6f0d0374c26668804520b90c
metasci <believewithyoureyes@gmail.com>
2020-05-08 18:31:52 -0400

    More stuff for the dash

--- dash/dash-public/src/modules/app/app-routing.module.ts
+++ dash/dash-public/src/modules/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { PageOneComponent } from './page-one/page-one.component'
 import { PageTwoComponent } from './page-two/page-two.component'
 import { PageThreeComponent } from './page-three/page-three.component'
 
+
 const routes: Routes = [
+  { path: '', redirectTo: '/one', pathMatch: 'full' },
   { path: 'one', component: PageOneComponent },
   { path: 'two', component: PageTwoComponent },
-  { path: 'three', component: PageThreeComponent },
+  { path: 'three', component: PageThreeComponent }
 ];
 
 @NgModule({
